29328583abe0342e6a654dd3454937e11b84782f
Basit ali <160397104+Basit1478@users.noreply.github.com>
2026-02-06 15:33:28 +0500

    Update next.config.mjs with local API URLs
    Added additional local API URLs to the environment variables.

--- Phase-3/frontend/next.config.mjs
+++ Phase-3/frontend/next.config.mjs
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://todo-chatbot-api-basit-2025.onrender.com",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://todo-chatbot-api-basit-2025.onrender.com","http://127.0.0.1:46473","http://localhost:46473"
   },
 };
 
 export default nextConfig;
+
 
